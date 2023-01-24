c379c8f55d558c91c2f42e3f04ad3106258ba69b
mikalikahi <sweeney.phil@gmail.com>
2023-01-24 13:01:42 +0800

    Starting up project to practice Caching with Spring Boot

---
+++ src/main/java/CacheDemo/CacheDemo/CacheDemoApplication.java
@@ -0,0 +1,13 @@
+package CacheDemo.CacheDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CacheDemo/CacheDemo/CacheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package CacheDemo.CacheDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
